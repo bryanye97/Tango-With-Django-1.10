c7c4a6ba69953372cfba77e82c192716afbe5ae3
Bryan <bryanye97@gmail.com>
2017-01-05 15:54:34 +1100

    Category pages linked up

--- src/rango/admin.py
+++ src/rango/admin.py
@@ -6,6 +6,7 @@ class PageAdmin(admin.ModelAdmin):
     list_display=('title', 'category', 'url')
 
 class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
     list_display=('name', 'views', 'likes')
 
 admin.site.register(Category, CategoryAdmin)
--- src/rango/urls.py
+++ src/rango/urls.py
@@ -3,5 +3,6 @@ from rango import views as rango_views
 
 urlpatterns = [
     url(r'^$', rango_views.index, name='index'),
-    url(r'^about/', rango_views.about, name='about'),
+    url(r'^about/$', rango_views.about, name='about'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$', rango_views.show_category, name='show_category'),
 ]

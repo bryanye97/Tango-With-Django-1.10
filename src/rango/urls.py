35621e7bb96c6bc76b99128a1d6f68906e6654e3
Bryan <bryanye97@gmail.com>
2017-01-06 16:28:15 +1100

    Added forms

--- src/rango/urls.py
+++ src/rango/urls.py
@@ -4,5 +4,7 @@ from rango import views as rango_views
 urlpatterns = [
     url(r'^$', rango_views.index, name='index'),
     url(r'^about/$', rango_views.about, name='about'),
+    url(r'^add_category/$', rango_views.add_category, name='add_category'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$', rango_views.show_category, name='show_category'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$', rango_views.add_page, name='add_page'),
 ]

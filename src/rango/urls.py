9e154258f6db98bf527c5fa740a7a3f937ed7bdf
Bryan <bryanye97@gmail.com>
2017-01-09 11:44:44 +1100

    Finished templates

--- src/rango/urls.py
+++ src/rango/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from rango import views as rango_views
 
+app_name = 'rango'
+
 urlpatterns = [
     url(r'^$', rango_views.index, name='index'),
     url(r'^about/$', rango_views.about, name='about'),

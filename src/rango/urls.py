1b9b731799791fb4d9f78966bdbe95be4e318dda
Bryan <bryanye97@gmail.com>
2017-01-10 16:06:15 +1100

    Added register view

--- src/rango/urls.py
+++ src/rango/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     url(r'^add_category/$', rango_views.add_category, name='add_category'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$', rango_views.show_category, name='show_category'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$', rango_views.add_page, name='add_page'),
+    url(r'^register/', rango_views.register, name='register'),
 ]
--- src/tango_with_django_project/settings.py
+++ src/tango_with_django_project/settings.py
@@ -109,6 +109,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+#Password hashers
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
